2c63298a21d00392bc5a97ca09f40a1382583a18
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-11-12 01:42:54 +0900

    アプリケーション作成

--- config/apps.rb
+++ config/apps.rb
@@ -33,4 +33,6 @@ Padrino.configure_apps do
 end
 
 # Mounts the core application for this project
+
+Padrino.mount('NiftyCup::Football', :app_file => Padrino.root('football/app.rb')).to('/football')
 Padrino.mount('NiftyCup::App', :app_file => Padrino.root('app/app.rb')).to('/')
